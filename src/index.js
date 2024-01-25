d689470bfb6be67ace7ed879c54bf1f9571e8f48
Ismail Ben seddik <ismail@Ismails-MacBook-Pro.local>
2024-01-25 16:20:13 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Arithmetic Service - Hello World!');
+});
+
+app.get('/add/:n/:m', (req, res) => {
+    res.json(Number(req.params.n) + Number(req.params.m));
+});
+
+app.listen(port);
+
